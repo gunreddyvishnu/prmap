df5df8c27e2fefcdb31edfac2f956aff6c11ca90
gunreddyvishnu <63645642+gunreddyvishnu@users.noreply.github.com>
2023-05-02 18:47:54 +0530

    This is a new commit for what I originally planned to be amended

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const express=require("express");
 const port = process.env.PORT || 3000;
 const app=express();
 
+var lat=17.3850;
+var lng=78.4867;
+
 
 app.get("/hello",function(req,res){
     console.log("request");
 if(req.query.lat!=undefined&&req.query.lng!=undefined){
     console.log(req.query.lat +","+ req.query.lng);
+    lat=req.query.lat;
+    lng=req.query.lng;
    
 res.status(200).send("");
 
@@ -19,7 +24,23 @@ else{
 
     // res.send(req.query.id);
 
-})
+});
 
+
+app.get("/getlats",function(req,res){
+
+   try{
+    res.send({
+        "lat":lat,
+        "lng":lng
+    })
+   }
+   catch(e){}
+   
+  
+
+
+
+})
 
 app.listen(port);
